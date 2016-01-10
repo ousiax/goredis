c2fcf1a1e8d8681e835f484146d0432ece510af8
qqbuby <qqbuby@gmail.com>
2016-01-10 19:29:40 +0800

    Update redis/redis.go
    * Support RESP string group

--- redis/redis.go
+++ redis/redis.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"net"
 	"strconv"
@@ -9,51 +10,27 @@ import (
 )
 
 type Client interface {
+	RESPStrings
 	Send(s string) (n int, err error)
-	RawReply() (resp []byte, err error)
-	Reply() (resp interface{}, err error)
-	Set(key, value []byte) (err error)
-	Get(key string) (value []byte)
+	Receive() (resp interface{}, err error)
+	RawReceive() (resp []byte, err error)
+	Close() (err error)
 }
 
 type client struct {
-	Conn   net.Conn
-	Writer *bufio.Writer
-	Reader *bufio.Reader
+	conn net.Conn
+	bw   *bufio.Writer
+	br   *bufio.Reader
 }
 
 func (c *client) Send(s string) (n int, err error) {
-	n, err = c.Writer.WriteString(s)
-	c.Writer.Flush()
+	n, err = c.bw.WriteString(s)
+	c.bw.Flush()
 	return
 }
 
-func (c *client) RawReply() ([]byte, error) {
-	resp, err := c.Reader.ReadSlice('\n')
-	if err != nil {
-		return resp, err
-	}
-	symbol := resp[0] // +,-,:,$,*
-	switch symbol {
-	case '+', '-', ':':
-		break
-	case '$':
-		line, _ := c.Reader.ReadSlice('\n')
-		resp = append(resp, line...)
-	case '*':
-		d := strings.Trim(string(resp), "*\r\n")
-		length, _ := strconv.Atoi(d)
-		for i := 0; i < length; i++ {
-			line, _ := c.RawReply()
-			resp = append(resp, line...)
-		}
-	default:
-		panic(fmt.Sprintf("Protocol Error: %s, %s", string(symbol), string(resp)))
-	}
-	return resp, err
-}
-func (c *client) Reply() (interface{}, error) {
-	line, err := c.Reader.ReadSlice('\n')
+func (c *client) Receive() (interface{}, error) {
+	line, err := c.br.ReadSlice('\n')
 	if err != nil {
 		return nil, err
 	}
@@ -68,7 +45,7 @@ func (c *client) Reply() (interface{}, error) {
 		if length == -1 {
 			return nil, nil
 		} else {
-			s, _ := c.Reader.ReadSlice('\n')
+			s, _ := c.br.ReadSlice('\n')
 			return string(s[0 : len(s)-2]), nil
 		}
 	case '*':
@@ -78,7 +55,7 @@ func (c *client) Reply() (interface{}, error) {
 		}
 		reslt := make([]interface{}, 0)
 		for i := 0; i < length; i++ {
-			rep, _ := c.Reply()
+			rep, _ := c.Receive()
 			reslt = append(reslt, rep)
 		}
 		return reslt, nil
@@ -87,22 +64,127 @@ func (c *client) Reply() (interface{}, error) {
 	}
 }
 
-func (c *client) Set(key, value []byte) (err error) {
-	return nil
+func (c *client) RawReceive() ([]byte, error) {
+	resp, err := c.br.ReadSlice('\n')
+	if err != nil {
+		return resp, err
+	}
+	symbol := resp[0] // +,-,:,$,*
+	switch symbol {
+	case '+', '-', ':':
+		break
+	case '$':
+		line, _ := c.br.ReadSlice('\n')
+		resp = append(resp, line...)
+	case '*':
+		d := strings.Trim(string(resp), "*\r\n")
+		length, _ := strconv.Atoi(d)
+		for i := 0; i < length; i++ {
+			line, _ := c.RawReceive()
+			resp = append(resp, line...)
+		}
+	default:
+		panic(fmt.Sprintf("Protocol Error: %s, %s", string(symbol), string(resp)))
+	}
+	return resp, err
 }
 
-func (c *client) Get(key string) (value []byte) {
-	//line, _ := c.br.ReadSlice('\n')
-	return nil
+func (c *client) Close() (err error) {
+	return c.conn.Close()
 }
 
-func NewClient(network, address string) (cmd Client, err error) {
+func (c *client) writeBytes(p []byte) error {
+	err := c.writeHeader('$', int64(len(p)))
+	if err != nil {
+		return err
+	}
+	_, err = c.bw.Write(p)
+	if err == nil {
+		_, err = c.bw.Write([]byte{'\r', '\n'})
+	}
+	return err
+}
+
+//  2\r\n
+func (c *client) writeInt(n int64) error {
+	var p []byte
+	p = strconv.AppendInt(p, n, 10)
+	return c.writeBytes(p)
+}
+
+func (c *client) writeFloat(f float64) error {
+	var p []byte
+	p = strconv.AppendFloat(p, f, 'g', -1, 64)
+	return c.writeBytes(p)
+}
+
+//  *2\r\n
+func (c *client) writeHeader(symbol byte, n int64) error {
+	err := c.bw.WriteByte(symbol)
+	if err == nil {
+		err = c.writeInt(n)
+	}
+	return err
+}
+
+//  LLEN\r\n
+func (c *client) writeString(s string) error {
+	return c.writeBytes([]byte(s))
+}
+
+func (c *client) executeCommand(commandName string, args ...interface{}) error {
+	err := c.writeHeader('*', int64(1+len(args)))
+	if err != nil {
+		return nil
+	}
+	err = c.writeString(commandName)
+	for _, arg := range args {
+		if err != nil {
+			break
+		}
+		switch arg.(type) {
+		case string:
+			err = c.writeString(arg.(string))
+		case []byte:
+			err = c.writeBytes(arg.([]byte))
+		case int:
+			err = c.writeInt(int64(arg.(int)))
+		case float32:
+			err = c.writeFloat(float64(arg.(float32)))
+		case float64:
+			err = c.writeFloat(arg.(float64))
+		case bool:
+			if arg.(bool) {
+				err = c.writeString("1")
+			} else {
+				err = c.writeString("0")
+			}
+		case nil:
+			err = c.writeString("")
+		default:
+			var buf bytes.Buffer
+			fmt.Fprint(&buf, arg)
+			err = c.writeBytes(buf.Bytes())
+		}
+	}
+	return err
+}
+
+func (c *client) Get(key string) (value interface{}, err error) {
+	err = c.executeCommand("GET", key)
+	if err == nil {
+		return c.Receive()
+	}
+	return nil, err
+}
+
+func NewClient(network, address string) (Client, error) {
 	conn, err := net.Dial(network, address)
 	if err != nil {
 		return nil, err
 	}
 	w := bufio.NewWriter(conn)
-	rd := bufio.NewReader(conn)
-	c := client{Conn: conn, Writer: w, Reader: rd}
-	return Client(&c), nil
+	r := bufio.NewReader(conn)
+	cli := client{conn: conn, bw: w, br: r}
+	return Client(&cli), nil
 }
