bfb44d6c8a67553dd3ddee17329330ee2c4b9e9d
qqbuby <qqbuby@gmail.com>
2016-01-30 22:20:38 +0800

    Add pubsub_test.go

---
+++ redis/pubsub_test.go
@@ -0,0 +1,36 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2016 Roy Xu
+
+package redis_test
+
+import (
+	"github.com/qqbuby/goredis/redis"
+	"reflect"
+	"testing"
+)
+
+func TestPubSub(t *testing.T) {
+	sc, err := redis.NewPubSub(url)
+	if err != nil {
+		t.Fatalf("Could not connect to Redis at %s: Connection refused.", url)
+	}
+	defer sc.Close()
+
+	pc, err := redis.NewPubSub(url)
+	if err != nil {
+		t.Fatalf("Could not connect to Redis at %s: Connection refused.", url)
+	}
+	defer pc.Close()
+
+	sc.Subscribe("c1")
+	r := sc.Receive()
+	if !reflect.DeepEqual(r, redis.SubMessage{Kind: "SUBSCRIBE", Channel: "c1", Num: 1}) {
+		t.Fatal("TestPubSub failure.")
+	}
+	pc.Publish("c1", "Hi")
+	m := sc.Receive()
+	if !reflect.DeepEqual(m, redis.Message{Channel: "c1", Text: "Hi"}) {
+		t.Fatal("TestPubSub failure.")
+	}
+}
